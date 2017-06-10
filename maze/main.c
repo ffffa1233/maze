ad170e2ccd4e2aee17e8763a287f35678f8f0e33
SeongCheol Park <seongcheolpark@SeongCheolui-MacBook-Pro.local>
2017-06-10 21:22:48 +0900

    Initial Commit

---
+++ maze/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  maze
+//
+//  Created by SeongCheol Park on 2017. 6. 10..
+//  Copyright © 2017년 SeongCheol Park. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
