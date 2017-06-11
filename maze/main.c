bcf2526b2a2a91e884d470081e0ebf47d5b03aa5
SeongCheol Park <seongcheolpark@SeongCheolui-MacBook-Pro.local>
2017-06-11 13:46:47 +0900

    renewal the source code

--- maze/main.c
+++ maze/main.c
@@ -9,67 +9,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define SIZE    (10)
-
-void print_maze(int maze[SIZE][SIZE]){
-    for(int i=0;i<SIZE;i++){
-        for(int j=0;j<SIZE;j++){
-            if(maze[i][j]!=-1){
-                printf("%2d",maze[i][j]);
-            }else{
-                printf(" *");
-            }
+void print_maze(int** maze, int row, int col){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            printf("%2d",maze[i][j]);
         }
         printf("\n");
     }
 }
 
-void find_start_position(int maze[SIZE][SIZE]){
-    for(int i=0;i<SIZE;i++){
-        for(int j=0;j<SIZE;j++){
-            if(maze[i][j] == 1){
-                printf("start position : %2d, %2d\n",i,j);
-            }
+void initialize_maze(int*** maze, int row, int col){
+    (*maze) = (int**)malloc(sizeof(int*)*row);
+    for(int i=0;i<col;i++){
+        (*maze)[i] = (int*)malloc(sizeof(int)*col);
+    }
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            (*maze)[i][j] = rand()%2;
         }
     }
 }
 
-void find_end_position(int maze[SIZE][SIZE]){
-    for(int i=0;i<SIZE;i++){
-        for(int j=0;j<SIZE;j++){
-            if(maze[i][j] == 2){
-                printf("end position : %2d, %2d\n",i,j);
-            }
-        }
-    }
+void find_path(int** maze, int row, int col){
+    printf("Program find the path\n");
 }
 
+void input_data(int *row, int *col){
+    printf("Please input row number :");
+    scanf("%d",row);
+    printf("Please input col number :");
+    scanf("%d",col);
+}
+
+void free_maze(int** maze, int row){
+    printf("Program free maze memory\n");
+    for(int i=0;i<row;i++){
+        free(maze[i]);
+    }
+    free(maze);
+}
 
 int main(void) {
+    /* show what this program is */
+    printf("*** finding path of maze ***\n");
     
-    /* initialize maze */
-    int maze[SIZE][SIZE] = {
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
-        {-1, 1, 0, 0,-1, 0, 0, 0, 0,-1},
-        {-1,-1,-1, 0,-1,-1,-1,-1, 0,-1},
-        {-1,-1, 0, 0, 0, 0, 0,-1, 0,-1},
-        {-1,-1,-1,-1, 0,-1,-1,-1, 0,-1},
-        {-1,-1, 0, 0, 0,-1, 0,-1, 0,-1},
-        {-1, 0, 0,-1, 0, 0, 0, 0, 0,-1},
-        {-1, 0,-1,-1,-1,-1, 0,-1, 0,-1},
-        {-1, 0, 0, 0, 0, 0,-1,-1, 2,-1},
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}
-    };
-
+    /* declare random function and variable */
+    srand((unsigned)time(NULL));
+    int row = 0;
+    int col = 0;
+    int **maze;
+    
+    /* input row and column number */
+    input_data(&row, &col);
+    
+    /* initialize(make) maze */
+    initialize_maze(&maze, row, col);
+    
     /* print maze */
-    print_maze(maze);
+    print_maze(maze, row, col);
     
-    /* start position number */
-    find_start_position(maze);
+    /* find path */
+    find_path(maze, row, col);
     
-    /* end position number */
-    find_end_position(maze);
+    /* free maze */
+    free_maze(maze, row);
     
     return 0;
 }
